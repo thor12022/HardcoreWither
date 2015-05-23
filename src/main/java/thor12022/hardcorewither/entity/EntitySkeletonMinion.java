6f1252c1bb51fbd7940983cbd2208d624158741b
thor12022 <thor12022@gmail.com>
2015-05-22 22:07:16 -0500

    Wither Skeleton Minions are have swords and other Wither Skeleton attributes now

--- src/main/java/thor12022/hardcorewither/entity/EntitySkeletonMinion.java
+++ src/main/java/thor12022/hardcorewither/entity/EntitySkeletonMinion.java
@@ -2,9 +2,14 @@ package thor12022.hardcorewither.entity;
 
 import thor12022.hardcorewither.ModInformation;
 import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.boss.EntityWither;
 import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
@@ -39,6 +44,17 @@ public class EntitySkeletonMinion extends EntitySkeleton
    protected void dropFewItems(boolean p_70628_1_, int p_70628_2_)
    {}
    
+   @Override
+   public IEntityLivingData onSpawnWithEgg(IEntityLivingData livingData)
+   {
+      // Apply the Wither Skeleton's standard attributes
+      this.tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.2D, false));
+      this.setCurrentItemOrArmor(0, new ItemStack(Items.stone_sword));
+      this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4.0D);
+      return livingData;
+   }
+   
+   @Override
    protected boolean isValidLightLevel()
    {
        return true;
